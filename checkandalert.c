8c413dfb115ab83aa8fa2a5affc6ece164bcf494
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-01-28 19:26:14 +0530

    Update checkandalert.c

--- checkandalert.c
+++ checkandalert.c
@@ -1,14 +1,14 @@
 #include "checkandalert.h"
 #include "stats.h"
-bool emailAlerter()
+void emailAlerter()
 {
   emailAlertCallCount = 1;
-  return (emailAlertCallCount==1);
+  //return (emailAlertCallCount==1);
 }
-bool ledAlerter()
+void ledAlerter()
 {
   ledAlertCallCount = 1;
-  return (ledAlertCallCount==1);
+  //return (ledAlertCallCount==1);
 }
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
