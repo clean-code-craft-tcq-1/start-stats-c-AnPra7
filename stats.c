6c16598de44e5023905ff52ed4bf8c660c77750d
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-01-28 17:56:50 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -15,22 +15,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
         sum+=numberset[i];
     }
     s.average = sum/setlength;
-    
-    //finding maximum
-   /* for (i=0; i<setlength; i++)
-   {
-   if (numberset[i]>s.max)
-   {
-        s.max=numberset[i];
-   }
-   }
-    
-     //finding minimum
-int c, index = 0;
- 
-  for (c = 1; c < setlength; c++)
-    if (numberset[c] < min)
-      index = c;*/
+    //finding minimum and maximum of a number
     s.max=s.min=numberset[0];
     for(i=1; i<setlength; i++)
     {
